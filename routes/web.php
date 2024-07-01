d47e1e8183f653036140d52bfebe3dae5d4a3594
Irfan Adi <irfan.adi1209@gmail.com>
2024-07-01 07:39:55 +0700

    5. Blade Component

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ["name" => "Irfan Adi", 'title' => 'About']);
+});
+
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
 });
